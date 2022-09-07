880978f84ec35b09e99b8232d8e5673a6a7f9e3d
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-07 19:56:03 +0530

    Update LC_1920.py

--- LC_1920.py
+++ LC_1920.py
@@ -14,3 +14,20 @@ def buildArray(nums):
 
 print(buildArray([0, 2, 1, 5, 3, 4]))
 print(buildArray([5, 0, 1, 2, 3, 4]))
+
+
+# same aproch
+def buildArray(nums):
+    n = len(nums)
+    
+    for i in range(len(nums)):
+        nums[i] = n * (nums[nums[i]] % n) + nums[i]
+        
+    for i in range(len(nums)):
+        nums[i] = nums[i] // n
+
+    return nums
+
+
+print(buildArray([0, 2, 1, 5, 3, 4]))
+print(buildArray([5, 0, 1, 2, 3, 4]))
