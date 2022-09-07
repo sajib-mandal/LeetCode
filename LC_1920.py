2e365867daea0d07e1c6047ea66a051d984ee6ad
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-07 20:06:27 +0530

    Update LC_1920.py

--- LC_1920.py
+++ LC_1920.py
@@ -31,3 +31,37 @@ def buildArray(nums):
 
 print(buildArray([0, 2, 1, 5, 3, 4]))
 print(buildArray([5, 0, 1, 2, 3, 4]))
+
+
+# same aproch
+
+def buildArray(nums):
+    n = len(nums)
+
+    for i in range(len(nums)):
+        nums[i] = n * (nums[nums[i]] % n) + nums[i]
+
+    for i in range(len(nums)):
+        nums[i] = int(nums[i] / n)
+
+    return nums
+
+
+print(buildArray([0, 2, 1, 5, 3, 4]))
+print(buildArray([5, 0, 1, 2, 3, 4]))
+
+
+
+# Time O(n)
+# Space O(n)
+
+
+def buildArray(nums):
+    res = []
+    for i in range(len(nums)):
+        res.append(nums[nums[i]])
+    return res
+
+
+print(buildArray([0, 2, 1, 5, 3, 4]))
+print(buildArray([5, 0, 1, 2, 3, 4]))
