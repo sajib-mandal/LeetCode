63a0c4d04d1cf41e11e9f0db5ed6766e3855683e
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-14 19:09:42 +0530

    Create levelorder.py

---
+++ levelorder.py
@@ -0,0 +1,49 @@
+import collections
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def levelOrder(root):
+    if root is None:
+        return []
+
+    queue = collections.deque([root])
+    res = []
+
+    while queue:
+        size = len(queue)
+        tmp = []
+
+        while size > 0:
+            node = queue.popleft()
+            tmp.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+            size -= 1
+        res.append(tmp)
+
+    return res
+
+
+a = Node(3)
+b = Node(9)
+c = Node(20)
+d = Node(15)
+e = Node(7)
+
+a.left = b
+a.right = c
+c.left = d
+c.right = e
+
+print(levelOrder(a))
