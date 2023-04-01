e4204e0130a55aeca27a12987bd8306ebe75ed0e
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2023-04-01 18:27:25 +0530

    Update 121.maxProfit.py

--- Easy/121.maxProfit.py
+++ Easy/121.maxProfit.py
@@ -1,3 +1,5 @@
+# Mysolution
+
 def maxProfit(prices):
     if not prices:
         return 0
@@ -14,3 +16,18 @@ def maxProfit(prices):
 
 print(maxProfit([7, 1, 5, 3, 6, 4])) # 5
 print(maxProfit([7, 6, 4, 3, 1])) # 0
+
+
+# needcode
+# Two pointer
+    l, r = 0, 1
+    maxP = 0
+    
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            maxP = max(maxP, profit)
+        else:
+            l = r
+        r += 1
+    return maxP
